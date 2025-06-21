baf7e9312cb9710f7ce1a0ceb1cf745584af0381
Sarangnm <107618860+Sarangnm@users.noreply.github.com>
2025-06-21 08:39:36 +0530

    Initial commit for products page

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
   const navbar = document.getElementById("navbar");
+  const navmenu = document.getElementById("navmenu");
   const icon = hamburger.querySelector("i");
 
   hamburger.addEventListener("click", () => {
     navbar.classList.toggle("active");
+    navmenu.classList.toggle("active")
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-bars-staggered")
   });
