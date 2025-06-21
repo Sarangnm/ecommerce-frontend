f2e4a2c87d5beda51c07463376c3be8a38b86433
Sarangnm <107618860+Sarangnm@users.noreply.github.com>
2025-06-21 06:54:40 +0530

    Hero section Added

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
   const navbar = document.getElementById("navbar");
+  const icon = hamburger.querySelector("i");
 
   hamburger.addEventListener("click", () => {
     navbar.classList.toggle("active");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-bars-staggered")
   });
 });
